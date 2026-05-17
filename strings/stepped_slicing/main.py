7f3450073b95dbc20c6d44e2b12a3dc57e06ebb2
mikeal1246-cyber <>
2026-05-17 17:02:55 +0000

    Stepped slicing

--- strings/stepped_slicing/main.py
+++ strings/stepped_slicing/main.py
@@ -1,5 +1,9 @@
 filename = "Annual Budget Report 2024"
 
-year_code = filename[21:24:2]
+# 1. Split on spaces and take the last piece, which is the year
+year = filename.split()[-1]       # "2024"
+
+# 2. Take every second character (indexes 0 and 2)
+year_code = year[::2]            # "22"
 
 print("Variable sliced_string equals:", year_code)
